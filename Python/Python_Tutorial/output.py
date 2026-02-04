630bf06aa13997e8f5e0c5318cc65ee8306a7749
harya06 <haryasanta@gmail.com>
2026-02-04 23:51:11 +0700

    first commit

---
+++ Python/Python_Tutorial/output.py
@@ -0,0 +1,20 @@
+#Menampilkan teks
+
+print("Hallo World\n")
+print("Hello World!" ,end = " ")
+print("I will print on the same line.")
+
+#Menampilkan paragraf
+print("""
+Nama : Harya
+Kelas: G-1
+NIM  : 24051030010
+Prodi: Teknik ELektro
+""")
+
+#Meampilkan angka
+print(1)
+print(1, 2, 3, 4, 5)
+
+angka = 19923
+print(angka)
